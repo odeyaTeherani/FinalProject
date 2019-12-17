d19466d50976d7a7e3a7864010977b3732c65a3a
davidbogomolv <davidbogomolov@gmail.com>
2019-12-17 10:02:32 +0200

    Event Skelaton

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { ReportComponent } from './report/report.component';
 import { SharedComponent } from './shared/shared.component';
 import { ComponentsComponent } from './shared/components/components.component';
 import { ServicesComponent } from './shared/services/services.component';
+import { EventsComponent } from './events/events.component';
+import { EventsFilterComponent } from './events/events-filter/events-filter.component';
+import { EventsDetailComponent } from './events/events-detail/events-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,11 +20,15 @@ import { ServicesComponent } from './shared/services/services.component';
     ReportComponent,
     SharedComponent,
     ComponentsComponent,
-    ServicesComponent
+    ServicesComponent,
+    EventsComponent,
+    EventsFilterComponent,
+    EventsDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/events/events-filter/events-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events-filter',
+  templateUrl: './events-filter.component.html',
+  styleUrls: ['./events-filter.component.scss']
+})
+export class EventsFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
