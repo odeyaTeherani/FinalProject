26682b8bec73523a937d84d1bdca31905e779654
davidbogomolv <davidbogomolov@gmail.com>
2020-01-25 19:11:20 +0200

    fridy fixes

---
+++ client/src/app/shared/components/sessions-layout/sessions-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sessions-layout',
+  templateUrl: './sessions-layout.component.html',
+  styleUrls: ['./sessions-layout.component.scss']
+})
+export class SessionsLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
