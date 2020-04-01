d932d52314b422cb1660b78666edf69fae832626
Odeya Teherani <odeyalosh123@gmail.com>
2020-04-01 11:33:40 +0300

    add

---
+++ client/src/app/setting/about-the-app/about-the-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-the-app',
+  templateUrl: './about-the-app.component.html',
+  styleUrls: ['./about-the-app.component.scss']
+})
+export class AboutTheAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
