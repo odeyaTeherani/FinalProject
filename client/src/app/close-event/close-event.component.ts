88ff2ae314e0c7396a80c6d4294555f975d6204b
Odeya Teherani <odeyalosh123@gmail.com>
2020-04-28 11:58:07 +0300

    close-event added

---
+++ client/src/app/close-event/close-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-close-event',
+  templateUrl: './close-event.component.html',
+  styleUrls: ['./close-event.component.scss']
+})
+export class CloseEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
