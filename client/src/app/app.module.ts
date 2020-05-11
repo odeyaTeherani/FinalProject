075f3a96348f0aece510c9cab4e62bec2318ddb1
Odeya Teherani <odeyalosh123@gmail.com>
2020-05-11 13:44:45 +0300

    close-event added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HomeComponent} from './home/home.component';
 import {HttpClientModule} from '@angular/common/http';
 import {SharedModule} from './shared/shared.module';
-import {InMemHeroService} from './shared/services/in-memory-db-service';
-import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
-import {environment} from '../environments/environment';
 
 
 @NgModule({
--- client/src/app/close-event/close-event.module.ts
+++ client/src/app/close-event/close-event.module.ts
@@ -3,13 +3,29 @@ import { CommonModule } from '@angular/common';
 import { CloseEventComponent } from './close-event.component';
 import {RouterModule} from '@angular/router';
 import {CloseEventRoutes} from './close-event-routing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FlexModule} from '@angular/flex-layout';
+import {MatButtonModule, MatDatepickerModule, MatIconModule, MatInputModule} from '@angular/material';
+import {GooglePlacesModule} from '../shared/directives/google-places/google-places.module';
+import {SharedModule} from '../shared/shared.module';
+import {SeverityLevelModule} from '../shared/components/severity-level/severity-level.module';
 
 
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(CloseEventRoutes)
+    RouterModule.forChild(CloseEventRoutes),
+    FormsModule,
+    FlexModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    GooglePlacesModule,
+    SharedModule,
+    SeverityLevelModule,
+    MatButtonModule,
+    MatIconModule
   ],
   declarations: [CloseEventComponent]
 })
