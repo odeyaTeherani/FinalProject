9adda221b09bff44d75885f5ff5c0c2e8ae302aa
davidbogomolv <davidbogomolov@gmail.com>
2020-06-17 12:18:01 +0300

    add system-values component

---
+++ client/src/app/system-values/system-values.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-system-values',
+  templateUrl: './system-values.component.html',
+  styleUrls: ['./system-values.component.scss']
+})
+export class SystemValuesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
