19900f83ad67cd41cd8802f7b1d5faf7ae5a1f8b
odeya teherani <davidbogomolov@gmail.com>
2020-07-16 17:16:12 +0300

    add theme file

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {OverlayContainer} from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+// use this to set correct theme class on app holder
+  // eg: <div [class]="themeClass">...</div>
+  themeClass: string;
+
+  constructor(
+    private overlayContainer: OverlayContainer
+  ) {
+  }
+
+  ngOnInit(): void {
+    // subscribe to some source of theme change events, then...
+    this.themeClass = 'my-light-blue-theme.scss';
+
+    // remove old theme class and add new theme class
+    // we're removing any css class that contains '-theme' string but your theme classes can follow any pattern
+    const overlayContainerClasses = this.overlayContainer.getContainerElement().classList;
+    const themeClassesToRemove = Array.from(overlayContainerClasses).filter((item: string) => item.includes('-theme'));
+    if (themeClassesToRemove.length) {
+      overlayContainerClasses.remove(...themeClassesToRemove);
+    }
+    overlayContainerClasses.add(this.themeClass);
+  }
+
 }
